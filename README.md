d7edc89944472ecb67d12c4c76d506f59f0bd0e1
Matt Stalzer <mjs58181@uga.edu>
2020-09-12 23:46:37 -0400

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Eat-Da-Burger
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  
+  ### Description: This project allows the user to add different types of hamburgers to the screen as well as move them to the eaten list. The technologies used are a MySQL database, MVC, Node.js, Express, and Handlebars. 
+  
+  ### Table of Contents: 
+  1. [Usage](#1.-usage:)   
+  2. [Questions](#2.-contact-me)   
+
+  ## 1. Usage:
+  * The project can be found here [here](https://desolate-stream-81871.herokuapp.com/)
+  ## 2. Contact Me:
+  * Email: mjstalzer@gmail.com
+  * GitHub Username: mjstalzer
+    
--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,26 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection(
+    {
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "password",
+      database: "burgers_db"
+    })
+};
 
 // Make connection.
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-  // Export connection for our ORM to use.
-  module.exports = connection;
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
